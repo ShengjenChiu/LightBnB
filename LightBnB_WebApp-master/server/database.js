c511c090a1bb6fcf5e8ee28c60cc212bb05a9263
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-07-02 23:23:36 -0600

    getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -99,8 +99,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+
+  return pool
+  .query(`
+    SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating 
+    FROM properties
+    JOIN reservations ON reservations.property_id = properties.id
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message)
+  }); 
+
 }
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
